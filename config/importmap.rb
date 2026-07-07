2ec38e14aa9e847575e3afc444b72e2a289ce5fa
BYUN, SungUk <ukits@CEnA.co.kr>
2026-07-07 12:48:08 +0000

    38443a4aae208df765641e006bca76a4fa21d132 커밋의 실수를 보정해 frontend 에러를 없앰

--- config/importmap.rb
+++ config/importmap.rb
@@ -14,6 +14,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin_all_from 'app/javascript/services', under: 'services'
 pin_all_from 'app/javascript/utils', under: 'utils'
 
 pin 'leaflet' # @1.9.4
